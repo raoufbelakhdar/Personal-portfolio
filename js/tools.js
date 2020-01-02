827728eded2bdd61ec7eb25f28b213e0cd3a06f7
raouf belakhdar <raoufbelakhdarph@gmail.com>
2020-01-02 01:52:03 +0100

    update

--- js/tools.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.navbar_toggle').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.navbar').toggleClass('active');
-        e.preventDefault();
-    });
-});
